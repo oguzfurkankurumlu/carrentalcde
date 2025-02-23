3695331d25326215b332fb3a9bc039ab9f3696c5
Oğuz Furkan Kurumlu <87278475+oguzfurkankurumlu@users.noreply.github.com>
2025-02-23 16:19:44 +0300

    msmsmsm

--- obj/Debug/net9.0/carrentalcde.AssemblyInfo.cs
+++ obj/Debug/net9.0/carrentalcde.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("carrentalcde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2010de95dab98e23688df2cae628834c04602844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c917d37ab1520bf64be0bde1b23719ea84cc07b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("carrentalcde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("carrentalcde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
